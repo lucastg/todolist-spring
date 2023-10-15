b22d3f1dd8a43dcd4cd249debc6882a5eb41343a
Lucas Tadeu <lucasu37@gmail.com>
2023-10-15 18:25:58 -0300

    Nova rota para listar todos os usuarios

--- src/main/java/br/com/lucastadeu/todolist/user/UserController.java
+++ src/main/java/br/com/lucastadeu/todolist/user/UserController.java
@@ -4,10 +4,9 @@ import at.favre.lib.crypto.bcrypt.BCrypt;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/users")
@@ -30,5 +29,10 @@ public class UserController {
 
         var userCreated = this.userRepository.save(userModel);
         return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
+    }
+
+    @GetMapping("/")
+    public List<UserModel> list() {
+        return userRepository.findAll();
     }
 }
